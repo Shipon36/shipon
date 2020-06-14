dfd8d5c533e298918a6b40a1393dac57c82682f7
Shipon36 <66916158+Shipon36@users.noreply.github.com>
2020-06-14 23:51:58 +0600

    Create Shipon.py

---
+++ Shipon.py
@@ -0,0 +1,21 @@
+# this is my command
+
+* pkg update
+
+* pkg upgrade
+
+* pkg install python
+
+* pkg install python2
+
+* pkg install git
+
+* pkg install requests
+
+* pkg install mechanize
+
+* git clone https://github.com/shipon36/shipon
+
+* cd shipon
+
+* python2 shipon.py
